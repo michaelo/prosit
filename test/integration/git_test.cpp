8e5ea5811b4d484e856719bb0c7df3b6d5451f68
Michael Odden <me@michaelodden.com>
2021-01-12 00:47:10 +0100

    Added initial git integration test

--- test/integration/git_test.cpp
+++ test/integration/git_test.cpp
@@ -10,6 +10,18 @@
 
 namespace fs = std::filesystem;
 
+TEST(FileTst, test_file)
+{
+    // Verify clone of remote git repo
+    ASSERT_EQ(basic_app_main_run("../test/integration/testfiles/git_remote.manifest"), App_Status_Code::OK);
+
+    // Verify update existing clone
+
+    // Verify checkout to different branch/tag
+
+    // Verify fail if local folder exists, but is not a rep
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
