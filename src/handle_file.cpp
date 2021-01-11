086ad2a4f0c441c5713eb134c2f698dd4a248acd
Michael Odden <me@michaelodden.com>
2021-01-12 00:20:40 +0100

    Minor fixes to handle_file

--- src/handle_file.cpp
+++ src/handle_file.cpp
@@ -15,13 +15,13 @@ App_Status_Code handle_file(Context *c, Manifest_Entry *e)
 {
     c->debug("About to copy %s to %s\n", e->src, e->dst, e->line_in_manifest);
     // TODO: Do modified-check before copy? Unless e.g. -f / !
-
-    fs::path src_canonical = fs::canonical(e->src);
+    std::error_code error_code; // To at least suppress exceptions
+    fs::path src_canonical = fs::weakly_canonical(e->src, error_code);
     size_t dst_len = strlen(e->dst);
     // std::error_code error_code; // TODO: get rid of std-exceptions.
 
     if(e->dst[strlen(e->dst)-1] == '/') {
-        assert(sizeof(e->dst) > dst_len+src_canonical.filename().string().length());
+        assert(sizeof(e->dst) > dst_len+src_canonical.filename().u8string().length());
         strcpy(&e->dst[strlen(e->dst)], (const char*)src_canonical.filename().u8string().c_str());
     }
 
@@ -30,15 +30,15 @@ App_Status_Code handle_file(Context *c, Manifest_Entry *e)
         return App_Status_Code::Error;
     }
 
-    fs::path dst_folder = fs::weakly_canonical(e->dst).parent_path();
+    fs::path dst_folder = fs::weakly_canonical(e->dst, error_code).parent_path();
 
-    if(!fs::exists(dst_folder) && !fs::create_directories(dst_folder))
+    if(!fs::exists(dst_folder, error_code) && !fs::create_directories(dst_folder, error_code))
     {
         c->error("Could not copy file: %s, could not create directory\n", e->src);
         return App_Status_Code::Error;
     }
 
-    if(!fs::copy_file(e->src, e->dst, fs::copy_options::overwrite_existing)) {
+    if(!fs::copy_file(e->src, e->dst, fs::copy_options::overwrite_existing, error_code)) {
         c->error("Could not copy file: %s\n", e->src);
         return App_Status_Code::Error;
     }
