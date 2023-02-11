2848cf3b7e45050629c5731ea9a53cbb57a8c0a9
Michael Odden <me@michaelodden.com>
2023-02-11 15:22:12 +0100

    httpclient: core retrieval functionality implemented.

--- src/clients/httpclient.zig
+++ src/clients/httpclient.zig
@@ -1,7 +1,6 @@
 // TODO: Make this a self-contained cURL-wrapper
 const std = @import("std");
-
-var is_inited: bool = false;
+const print = std.debug.print;
 
 const cURL = @cImport({
     @cInclude("curl/curl.h");
@@ -28,75 +27,188 @@ pub const HttpMethod = enum {
 
 pub const RequestResponseType = enum { Error, Ok };
 
-pub const RequestResponse = union(RequestResponseType) {
-    // http code >= 400
-    Error: struct {
-        http_code: usize,
-        // headers ?
-        body: ?[]const u8, // TODO: allocate, and leave to receiver?
-    },
-    // http code < 400
-    Ok: struct {
-        http_code: usize,
-        // headers ?
-        body: ?[]const u8, // TODO: allocate, and leave to receiver?
-    },
+pub const RequestResponse = struct {
+    response_type: RequestResponseType,
+    headers: ?std.ArrayList(u8),
+    body: ?std.ArrayList(u8),
+    http_code: usize,
+    time: i64,
+
+    fn deinit(self: @This()) void {
+        if (self.body) |v| v.deinit();
+        if (self.headers) |v| v.deinit();
+    }
+
+    fn getHeader(self: @This(), comptime header: []const u8) ![]const u8 {
+        var key = header ++ ":";
+        if (self.headers) |aheaders| {
+            if (std.ascii.indexOfIgnoreCase(aheaders.items, key)) |idx| {
+                // Parse until eol. Not standards-compliant right now as headers are really allowed to span multiple lines.
+                if (std.mem.indexOfPos(u8, aheaders.items, idx, "\r\n")) |eol| {
+                    return std.mem.trim(u8, aheaders.items[idx + key.len .. eol], " \t");
+                } else {
+                    return error.InvalidHdeader;
+                }
+            } else {
+                return error.HeaderNotFound;
+            }
+        } else {
+            return error.HeadersNotCaptured;
+        }
+    }
+
+    fn contentType(self: @This()) ![]const u8 {
+        return self.getHeader("Content-Type");
+    }
 };
 
+fn writeToArrayListCallback(data: *anyopaque, size: c_uint, nmemb: c_uint, user_data: *anyopaque) callconv(.C) c_uint {
+    var buffer = @intToPtr(*std.ArrayList(u8), @ptrToInt(user_data));
+    var typed_data = @intToPtr([*]u8, @ptrToInt(data));
+    buffer.appendSlice(typed_data[0 .. nmemb * size]) catch return 0;
+    return nmemb * size;
+}
+
+fn boolToInt(val: bool) usize {
+    if (val) return 1;
+    return 0;
+}
+
 /// Main API
-pub fn request(allocator: std.mem.Allocator, method: HttpMethod, url: [:0]const u8, comptime params: struct { headers: ?[][:0]const u8 = null, follow_redirect: bool = false, insecure: bool = false }) error{MalformedInput}!RequestResponse {
-    _ = allocator;
-    _ = method;
-    _ = url;
-    _ = headers;
-    _ = params;
+pub fn request(allocator: std.mem.Allocator, method: HttpMethod, url: [:0]const u8, comptime params: struct {
+    headers: ?[][:0]const u8 = null,
+    follow_redirect: bool = true,
+    insecure: bool = false,
+    store_headers: bool = true,
+    store_body: bool = true,
+    verbose: bool = false,
+}) !RequestResponse {
     if (is_inited) return error.NotInited;
 
+    //////////////////////////////
+    // Init / generic setup
+    //////////////////////////////
+    const handle = cURL.curl_easy_init() orelse return error.CURLHandleInitFailed;
+    defer cURL.curl_easy_cleanup(handle);
+
+    var body_payload: ?std.ArrayList(u8) = null;
+    var headers_payload: ?std.ArrayList(u8) = null;
+
+    if (params.store_body) {
+        body_payload = std.ArrayList(u8).init(allocator);
+        errdefer body_payload.?.deinit();
+
+        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_WRITEDATA, &body_payload) != cURL.CURLE_OK) {
+            return error.CouldNotSetWriteCallback;
+        }
+    }
+
+    if (params.store_headers) {
+        headers_payload = std.ArrayList(u8).init(allocator);
+        errdefer headers_payload.?.deinit();
+
+        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_HEADERDATA, &headers_payload) != cURL.CURLE_OK) {
+            return error.CouldNotSetWriteCallback;
+        }
+    }
+
     // Initiate
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_CUSTOMREQUEST, HttpMethod.string(method)) != cURL.CURLE_OK) {
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_CUSTOMREQUEST, HttpMethod.string(method).ptr) != cURL.CURLE_OK) {
         return error.CouldNotSetRequestMethod;
     }
 
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_URL, url) != cURL.CURLE_OK) {
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_URL, url.ptr) != cURL.CURLE_OK) {
         return error.CouldNotSetURL;
     }
 
     // insecure?
-    if (params.insecure) {
-        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_SSL_VERIFYPEER, @intCast(c_long, 0)) != cURL.CURLE_OK) {
-            return error.CouldNotSetSslVerifyPeer;
-        }
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_SSL_VERIFYPEER, @intCast(c_long, boolToInt(params.insecure))) != cURL.CURLE_OK) {
+        return error.CouldNotSetSslVerifyPeer;
+    }
 
-        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_SSL_VERIFYHOST, @intCast(c_long, 0)) != cURL.CURLE_OK) {
-            return error.CouldNotSetSslVerifyHost;
-        }
+    // TODO: Now headers will currently be appended for all requests. Need to clean headers when redirected...
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_FOLLOWLOCATION, @intCast(c_long, boolToInt(params.follow_redirect))) != cURL.CURLE_OK) {
+        return error.CouldNotSetFollow;
+    }
+
+    // Verbosity
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_VERBOSE, @intCast(c_long, boolToInt(params.verbose))) != cURL.CURLE_OK) {
+        return error.CouldNotSetVerbose;
     }
 
     // Pass headers
     var list: ?*cURL.curl_slist = null;
     defer cURL.curl_slist_free_all(list);
 
-    var header_scrap_buf = initBoundedArray(u8, types.HttpHeader.MAX_VALUE_LEN);
     if (params.headers) |headers| {
         for (headers) |header| {
-            list = cURL.curl_slist_append(list, header);
+            list = cURL.curl_slist_append(list, header.ptr);
         }
         if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_HTTPHEADER, list) != cURL.CURLE_OK) {
             return error.CouldNotSetHeaders;
         }
     }
 
+    // Payload
+    //     if (method == .POST or method == .PUT or params.payload.len > 0) {
+    //         if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_POSTFIELDSIZE, params.payload.len) != cURL.CURLE_OK)
+    //             return error.CouldNotSetPostDataSize;
+    //         if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_POSTFIELDS, params.payload.ptr) != cURL.CURLE_OK)
+    //             return error.CouldNotSetPostData;
+    //     }
+
     // Configure response handling
+    if (params.store_body or params.store_headers) {
+        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_WRITEFUNCTION, writeToArrayListCallback) != cURL.CURLE_OK) {
+            return error.CouldNotSetWriteCallback;
+        }
+    }
 
     // Execute
+    var time_start = std.time.milliTimestamp();
+    if (cURL.curl_easy_perform(handle) != cURL.CURLE_OK) {
+        return error.FailedToPerformRequest;
+    }
+    var time = std.time.milliTimestamp() - time_start;
 
-
+    ////////////////////////
+    // Handle results
+    ////////////////////////
     // Retrieve response code
-
     // Evaluate OK-ish/error-ish and return
+    var http_code: u64 = 0;
+    if (cURL.curl_easy_getinfo(handle, cURL.CURLINFO_RESPONSE_CODE, &http_code) != cURL.CURLE_OK)
+        return error.CouldNotGetResponseCode;
 
-    return error.Woops;
+    if (http_code == 0) return error.Woops;
+
+    return RequestResponse{
+        .response_type = if (http_code < 400) .Ok else .Error,
+        .http_code = http_code,
+        .headers = headers_payload,
+        .body = body_payload,
+        .time = time,
+    };
 }
+
+test "httpclient.request" {
+    var result = try request(std.testing.allocator, .GET, "https://raw.githubusercontent.com/michaelo/_prosit_itest/main/README.md", .{});
+    defer result.deinit();
+    
+    switch (result.response_type) {
+        .Error => {
+            print("Got error\n", .{});
+        },
+        .Ok => {
+            print("Got OK\n", .{});
+        },
+    }
+    // print("Content-Type: {s}\n", .{try result.contentType()});
+    print("headers:\n{s}\n", .{result.headers.?.items});
+    print("body:\n{s}\n", .{result.body.?.items});
+}
+
+var is_inited: bool = false;
 
 pub fn init() !void {
     if (is_inited) return error.AlreadyInit;
@@ -107,142 +219,9 @@ pub fn init() !void {
 }
 
 pub fn deinit() void {
+    if (!is_inited) return;
     cURL.curl_global_cleanup();
     is_inited = false;
-}
-
-//////////////////////////
-// Original from sapt:
-//////////////////////////
-
-// Convenience-function to initiate a bounded-array without inital size of 0, removing the error-case brough by .init(size)
-pub fn initBoundedArray(comptime T: type, comptime capacity: usize) std.BoundedArray(T, capacity) {
-    return std.BoundedArray(T, capacity).init(0) catch unreachable;
-}
-
-/// Att! This adds a terminating zero at current .slice().len if there's capacity.
-/// Capacity must be > 0
-/// If not sufficient capacity: return null?
-pub fn boundedArrayAsCstr(comptime capacity: usize, array: *std.BoundedArray(u8, capacity)) ![*]u8 {
-    std.debug.assert(capacity > 0);
-    if (array.constSlice().len >= capacity) return error.Overflow;
-
-    array.buffer[array.constSlice().len] = 0;
-    return array.slice().ptr;
-}
-
-fn writeToBoundedArrayCallback(data: *anyopaque, size: c_uint, nmemb: c_uint, user_data: *anyopaque) callconv(.C) c_uint {
-    var buffer = @intToPtr(*std.BoundedArray(u8, 1024 * 1024), @ptrToInt(user_data));
-    var typed_data = @intToPtr([*]u8, @ptrToInt(data));
-    buffer.appendSlice(typed_data[0 .. nmemb * size]) catch return 0;
-    return nmemb * size;
-}
-
-pub const ProcessArgs = struct {
-    ssl_insecure: bool = false,
-    verbose: bool = false,
-};
-
-/// Primary worker function performing the request and handling the response
-pub fn processEntry(entry: *types.Entry, args: ProcessArgs, result: *types.EntryResult) !void {
-    if (is_inited) return error.NotInited;
-
-    //////////////////////////////
-    // Init / generic setup
-    //////////////////////////////
-    const handle = cURL.curl_easy_init() orelse return error.CURLHandleInitFailed;
-    defer cURL.curl_easy_cleanup(handle);
-
-    ///////////////////////
-    // Setup curl options
-    ///////////////////////
-
-    // Set HTTP method
-    var method = initBoundedArray(u8, 8);
-    try method.appendSlice(entry.method.string());
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_CUSTOMREQUEST, try boundedArrayAsCstr(method.buffer.len, &method)) != cURL.CURLE_OK)
-        return error.CouldNotSetRequestMethod;
-
-    // Set URL
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_URL, try boundedArrayAsCstr(entry.url.buffer.len, &entry.url)) != cURL.CURLE_OK)
-        return error.CouldNotSetURL;
-
-    // Set Payload (if given)
-    if (entry.method == .POST or entry.method == .PUT or entry.payload.slice().len > 0) {
-        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_POSTFIELDSIZE, entry.payload.slice().len) != cURL.CURLE_OK)
-            return error.CouldNotSetPostDataSize;
-        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_POSTFIELDS, try boundedArrayAsCstr(entry.payload.buffer.len, &entry.payload)) != cURL.CURLE_OK)
-            return error.CouldNotSetPostData;
-    }
-
-    // Debug
-    if (args.verbose) {
-        std.debug.print("Payload: {s}\n", .{entry.payload.slice()});
-        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_VERBOSE, @intCast(c_long, 1)) != cURL.CURLE_OK)
-            return error.CouldNotSetVerbose;
-    }
-
-    if (args.ssl_insecure) {
-        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_SSL_VERIFYPEER, @intCast(c_long, 0)) != cURL.CURLE_OK)
-            return error.CouldNotSetSslVerifyPeer;
-
-        if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_SSL_VERIFYHOST, @intCast(c_long, 0)) != cURL.CURLE_OK)
-            return error.CouldNotSetSslVerifyHost;
-    }
-
-    // Pass headers
-    var list: ?*cURL.curl_slist = null;
-    defer cURL.curl_slist_free_all(list);
-
-    var header_scrap_buf = initBoundedArray(u8, types.HttpHeader.MAX_VALUE_LEN);
-    for (entry.headers.slice()) |*header| {
-        try header_scrap_buf.resize(0);
-        try header.render(header_scrap_buf.buffer.len, &header_scrap_buf);
-        list = cURL.curl_slist_append(list, try boundedArrayAsCstr(header_scrap_buf.buffer.len, &header_scrap_buf));
-    }
-
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_HTTPHEADER, list) != cURL.CURLE_OK)
-        return error.CouldNotSetHeaders;
-
-    //////////////////////
-    // Execute
-    //////////////////////
-    try result.response_first_1mb.resize(0);
-    try result.response_headers_first_1mb.resize(0);
-
-    // set write function callbacks
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_WRITEFUNCTION, writeToBoundedArrayCallback) != cURL.CURLE_OK)
-        return error.CouldNotSetWriteCallback;
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_HEADERDATA, &result.response_headers_first_1mb) != cURL.CURLE_OK)
-        return error.CouldNotSetWriteCallback;
-    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_WRITEDATA, &result.response_first_1mb) != cURL.CURLE_OK)
-        return error.CouldNotSetWriteCallback;
-
-    // TODO: This is the critical part to time
-    // Perform
-    // var time_start = std.time.milliTimestamp();
-    if (cURL.curl_easy_perform(handle) != cURL.CURLE_OK)
-        return error.FailedToPerformRequest;
-    // std.debug.print("inner time: {d}\n", .{std.time.milliTimestamp() - time_start});
-
-    ////////////////////////
-    // Handle results
-    ////////////////////////
-    var http_code: u64 = 0;
-    if (cURL.curl_easy_getinfo(handle, cURL.CURLINFO_RESPONSE_CODE, &http_code) != cURL.CURLE_OK)
-        return error.CouldNewGetResponseCode;
-
-    result.response_http_code = http_code;
-
-    var content_type_ptr: [*c]u8 = null;
-    if (cURL.curl_easy_getinfo(handle, cURL.CURLINFO_CONTENT_TYPE, &content_type_ptr) != cURL.CURLE_OK)
-        return error.CouldNewGetResponseContentType;
-
-    // Get Content-Type
-    if (content_type_ptr != null) {
-        var content_type_slice = try std.fmt.bufPrint(&result.response_content_type.buffer, "{s}", .{content_type_ptr});
-        try result.response_content_type.resize(content_type_slice.len);
-    }
 }
 
 pub fn httpCodeToString(code: u64) []const u8 {
