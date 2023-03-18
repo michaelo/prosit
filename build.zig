c6352c2e118b739e5f5f19feae80a24e448fce0e
Michael Odden <me@michaelodden.com>
2023-03-18 16:09:20 +0100

    Added initial exploration-results for crossbuilding. Currently lacking platform-specific includes (ie. socket etc)

--- build.zig
+++ build.zig
@@ -84,6 +84,51 @@ pub fn build(b: *std.Build) void {
 
         const itest_step = b.step("itest", "Run default integration test suite");
         itest_step.dependOn(&exe_itests.step);
+
+        // crossBuild(b, optimize) catch |e| {
+        //     std.debug.print("Got error while crossbuilding: {s}\n", .{@errorName(e)});
+        // };
+
+    }
+}
+
+
+// Not working until we resolve/vendor platform-specific referencedy by curl-includes
+fn crossBuild(b: *std.Build, optimize: std.builtin.Mode) !void {
+    // Create job to generate crossbuilds for all desired targets
+    // .target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-freestanding" }),
+    var prevStep: ?*std.build.Step = null;
+    inline for([_][]const u8{ "x86_64-windows-gnu", "x86_64-macos", "x86_64-linux-gnu" }) |xbuild_target| {
+        const tmpexe = b.addExecutable(.{
+            .name = "prosit",
+            // In this case the main source file is merely a path, however, in more
+            // complicated build scripts, this could be a generated file.
+            .root_source_file = .{ .path = "src/main.zig" },
+            .target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = xbuild_target }),
+            .optimize = optimize,
+        });
+        tmpexe.addIncludePath("vendored/includes");
+        try tmpexe.lib_paths.insert(0, std.Build.FileSource.relative("vendored/libs/" ++ xbuild_target));
+        // tmpexe.addLibraryPath("vendored/libs/" ++ xbuild_target);
+        tmpexe.linkSystemLibrary("libcurl");
+
+        tmpexe.setMainPkgPath(".");
+        tmpexe.output_dir = "xbuild/" ++ xbuild_target;
+
+        // This declares intent for the executable to be installed into the
+        // standard location when the user invokes the "install" step (the default
+        // step when running `zig build`).
+        tmpexe.install();
+
+        if(prevStep) |prev| {
+            tmpexe.step.dependOn(prev);
+        }
+
+        prevStep = &tmpexe.step;
     }
 
+    if(prevStep) |prev| {
+        const xbuild_step = b.step("crossbuild", "Crossbuild for a set of predefined targets");
+        xbuild_step.dependOn(prev);
+    }
 }
